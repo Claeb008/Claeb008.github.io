2fde854cb7632b95fdd35fc354400df6539f52fc
Claeb008 <37882626+Claeb008@users.noreply.github.com>
2018-11-18 21:22:44 -0500

    asd

--- objs/OrbitControls.js
+++ objs/OrbitControls.js
@@ -736,7 +736,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 		if ( scope.enabled === false ) return;
 
-		event.preventDefault();
+		//event.preventDefault();
 
 		switch ( state ) {
 
