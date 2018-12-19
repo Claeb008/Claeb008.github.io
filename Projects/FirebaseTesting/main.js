90a001a1a98026ed8b948caba616b568482b4cd9
Claeb008 <37882626+Claeb008@users.noreply.github.com>
2018-12-19 17:47:29 -0500

    sd

--- Projects/FirebaseTesting/main.js
+++ Projects/FirebaseTesting/main.js
@@ -251,7 +251,7 @@ var uiConfig = {
       // User successfully signed in.
       // Return type determines whether we continue the redirect automatically
       // or whether we leave that to developer to handle.
-      return true, 'index.html';
+      return false, 'index.html';
     },
     uiShown: function() {
       // The widget is rendered.
